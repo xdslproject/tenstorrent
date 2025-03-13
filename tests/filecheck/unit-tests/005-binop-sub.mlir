6debe36a8da9b8e14bcf2b0c24bdb56854ed91c3
Jake <jakedves@gmail.com>
2025-03-13 20:12:22 +0000

    implement unit test for subtraction

---
+++ tests/filecheck/unit-tests/005-binop-sub.mlir
@@ -0,0 +1,20 @@
+// RUN: python3.13 tenstorrent/tools/tt-opt %s -t tt-metalium | filecheck %s
+
+builtin.module {
+  builtin.module attributes {kernel_type = "host"} {
+    func.func @main() -> i32 {
+      %0 = arith.constant 2 : i32
+      %1 = arith.constant 3 : i32
+      %2 = arith.subi %0, %1 : i32
+      %a = memref.alloc() : memref<i32>
+      memref.store %2, %a[] : memref<i32>
+      %3 = arith.constant 0 : i32
+      func.return %3 : i32
+    }
+  }
+}
+
+// CHECK:      std::int32_t main() {
+// CHECK-NEXT:     std::int32_t a = 2 - 3;
+// CHECK-NEXT:     return 0;
+// CHECK-NEXT: }
--- tests/filecheck/unit-tests/005-binop-sub.py
+++ tests/filecheck/unit-tests/005-binop-sub.py
@@ -3,15 +3,15 @@ from tenstorrent.frontend import tt
 
 
 @tt.host
-def simple_binop_add():
-    a = 2 + 3
+def simple_binop_sub():
+    a = 2 - 3
 
 # CHECK:      builtin.module {
 # CHECK-NEXT:   builtin.module attributes {kernel_type = "host"} {
 # CHECK-NEXT:     func.func @main() -> i32 {
 # CHECK-NEXT:       %0 = arith.constant 2 : i32
 # CHECK-NEXT:       %1 = arith.constant 3 : i32
-# CHECK-NEXT:       %2 = arith.addi %0, %1 : i32
+# CHECK-NEXT:       %2 = arith.subi %0, %1 : i32
 # CHECK-NEXT:       %a = memref.alloc() : memref<i32>
 # CHECK-NEXT:       memref.store %2, %a[] : memref<i32>
 # CHECK-NEXT:       %3 = arith.constant 0 : i32
