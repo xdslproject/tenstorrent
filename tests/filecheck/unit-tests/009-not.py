2aafebf04d70d9893017deb59a7fa74bc3786f95
Jake <jakedves@gmail.com>
2025-03-13 20:43:41 +0000

    implement test case for `not` operator

---
+++ tests/filecheck/unit-tests/009-not.py
@@ -0,0 +1,21 @@
+# RUN: python3.13 %s | filecheck %s
+from tenstorrent.frontend import tt
+
+
+@tt.host
+def nope():
+    a = not True
+
+# CHECK:      builtin.module {
+# CHECK-NEXT:   builtin.module attributes {kernel_type = "host"} {
+# CHECK-NEXT:     func.func @main() -> i32 {
+# CHECK-NEXT:       %0 = arith.constant true
+# CHECK-NEXT:       %1 = arith.constant true
+# CHECK-NEXT:       %2 = arith.xori %0, %1 : i1
+# CHECK-NEXT:       %a = memref.alloc() : memref<i1>
+# CHECK-NEXT:       memref.store %2, %a[] : memref<i1>
+# CHECK-NEXT:       %3 = arith.constant 0 : i32
+# CHECK-NEXT:       func.return %3 : i32
+# CHECK-NEXT:     }
+# CHECK-NEXT:   }
+# CHECK-NEXT: }
