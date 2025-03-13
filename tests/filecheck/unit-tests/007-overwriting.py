136362b9d73f9881645b49e9d93c4d7a888cdf53
Jake <jakedves@gmail.com>
2025-03-13 20:35:42 +0000

    implement test case for writing over variable

---
+++ tests/filecheck/unit-tests/007-overwriting.py
@@ -0,0 +1,24 @@
+# RUN: python3.13 %s | filecheck %s
+from tenstorrent.frontend import tt
+
+
+@tt.host
+def overwriting():
+    a = 5
+    a = a + 1
+
+# CHECK:      builtin.module {
+# CHECK-NEXT:   builtin.module attributes {kernel_type = "host"} {
+# CHECK-NEXT:     func.func @main() -> i32 {
+# CHECK-NEXT:       %0 = arith.constant 5 : i32
+# CHECK-NEXT:       %a = memref.alloc() : memref<i32>
+# CHECK-NEXT:       memref.store %0, %a[] : memref<i32>
+# CHECK-NEXT:       %1 = memref.load %a[] : memref<i32>
+# CHECK-NEXT:       %2 = arith.constant 1 : i32
+# CHECK-NEXT:       %3 = arith.addi %1, %2 : i32
+# CHECK-NEXT:       memref.store %3, %a[] : memref<i32>
+# CHECK-NEXT:       %4 = arith.constant 0 : i32
+# CHECK-NEXT:       func.return %4 : i32
+# CHECK-NEXT:     }
+# CHECK-NEXT:   }
+# CHECK-NEXT: }
