8fb0561e63ddcd9ef4fa34abaaa7a22462819a38
Jake <jakedves@gmail.com>
2025-03-20 11:00:17 +0000

    add shared dialect with constexpr handling

---
+++ tenstorrent/dialects/ttshared.py
@@ -0,0 +1,50 @@
+from typing import Generic
+
+from xdsl.dialects.builtin import i1, i32, IntegerType, Signedness, ContainerType
+from xdsl.ir import SSAValue, Operation, Dialect, AttributeCovT, ParametrizedAttribute, TypeAttribute
+from xdsl.irdl import IRDLOperation, irdl_attr_definition, operand_def, result_def, irdl_op_definition
+
+uint32 = IntegerType(32, signedness=Signedness.UNSIGNED)
+
+
+@irdl_attr_definition
+class ConstExprType(
+    Generic[AttributeCovT],
+    ParametrizedAttribute,
+    TypeAttribute,
+    ContainerType[AttributeCovT],
+):
+    name = "constexpr"
+
+    element_type = operand_def(AttributeCovT)
+
+    def __init__(
+        self,
+        element_type: AttributeCovT,
+    ) -> None:
+        super().__init__([element_type])
+
+    def get_element_type(self) -> AttributeCovT:
+        return self.element_type
+
+
+@irdl_op_definition
+class GetCompileTimeArgVal(IRDLOperation):
+    name = "ttshared.get_compile_time_arg_val"
+
+    index = operand_def(i1)
+    result = result_def(ConstExprType[uint32])
+
+    def __init__(self, i):
+        super().__init__(operands=[i], result_types=[ConstExprType(uint32)])
+
+
+TTShared = Dialect(
+    "ttshared",
+    [
+        GetCompileTimeArgVal,
+    ],
+    [
+        ConstExprType,
+    ],
+)
