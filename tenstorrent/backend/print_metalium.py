c86573a868384163ada11a3abddb7364ca5c78c2
Jake Davies <jakedves@gmail.com>
2025-04-16 18:21:59 +0100

    import matmul in compute kernel

--- tenstorrent/backend/print_metalium.py
+++ tenstorrent/backend/print_metalium.py
@@ -206,6 +206,9 @@ class PrintMetalium:
                     self.print("#include <cstdint>", indented=True, end="\n")
                     # TODO: generalise based on code possible? MLIR ops for include? Pass that adds these?
                     self.print(
+                        '#include "compute_kernel_api/matmul.h'
+                    )
+                    self.print(
                         '#include "compute_kernel_api/tile_move_copy.h"',
                         indented=True,
                         end="\n",
